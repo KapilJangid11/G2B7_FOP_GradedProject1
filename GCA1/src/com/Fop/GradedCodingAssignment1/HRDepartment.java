3c90f7bc1769b7cd740c2bdcd55424eb1812512b
KapilJangid11 <124651228+KapilJangid11@users.noreply.github.com>
2023-03-19 14:33:03 +0530

    Add files via upload

---
+++ GCA1/src/com/Fop/GradedCodingAssignment1/HRDepartment.java
@@ -0,0 +1,16 @@
+package com.Fop.GradedCodingAssignment1;
+
+public class HRDepartment extends SuperDepartment {
+	 public String departmentName() {
+	     return "HR Department";
+	 }
+	 public String getTodaysWork() {
+	     return "Fill today's timesheet and mark your attendance";
+	 }
+	 public String getWorkDeadline() {
+	     return "Complete by EOD";
+	 }
+	 public String doActivity() {
+	     return "team Lunch";
+	 }
+	}
